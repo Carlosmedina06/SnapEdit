d39942b9c18cc71411732fdecad3f2baee409817
carlosmedina06 <slaawmedina14@gmail.com>
2023-04-08 17:07:43 -0300

    reduzco codigo duplicado

--- src/components/cropImage/CropImage.jsx
+++ src/components/cropImage/CropImage.jsx
@@ -1,16 +1,17 @@
 import ReactCrop from 'react-image-crop'
 
 import UploadImg from '../uploadImg/UploadImg'
+import { Button } from '../Button/Button'
 
-const CropImage = ({imageCrop, crop, state, setDetails, setCrop, imageHandle}) => {
+const CropImage = ({ imageCrop, crop, state, setDetails, setCrop, imageHandle }) => {
   return (
     <div className="flex flex-col justify-center items-center">
-      <div className="flex flex-col justify-center  w-96 min-h-[400px] items-center border-dashed rounded-lg border-2 mb-5">
+      <div className="flex flex-col justify-center  w-64 sm:w-96 min-h-[350px] items-center border-dashed rounded-lg border-2 mb-5">
         {state.image ? (
           <ReactCrop crop={crop} onChange={(c) => setCrop(c)}>
             <img
               alt="imagen selected"
-              className="w-96 object-cover rounded-lg"
+              className="w-64  sm:w-96  object-cover rounded-lg"
               src={state.image}
               style={{
                 filter: `brightness(${state.brightness}%) brightness(${state.brightness}%) sepia(${state.sepia}%) saturate(${state.saturate}%) contrast(${state.contrast}%) grayscale(${state.grayscale}%) hue-rotate(${state.hueRotate}deg)`,
@@ -23,16 +24,7 @@ const CropImage = ({imageCrop, crop, state, setDetails, setCrop, imageHandle}) =
           <UploadImg imageHandle={imageHandle} />
         )}
       </div>
-      <div className="mb-2">
-        {crop && (
-          <button
-            className="rounded-lg px-4 py-2 capitalize text-white text-sm bg-violet-500 hover:bg-violet-600 active:bg-violet-700 focus:outline-none focus:ring focus:ring-violet-300"
-            onClick={imageCrop}
-          >
-            Crop Image
-          </button>
-        )}
-      </div>
+      <div className="mb-2">{crop && <Button onClick={imageCrop}>Crop Image</Button>}</div>
     </div>
   )
 }
