002170f9755ea6342b762c8972ed5c82b4d02728
carlosmedina06 <slaawmedina14@gmail.com>
2023-04-08 17:08:11 -0300

    format commit

--- src/components/rotate/Rotate.jsx
+++ src/components/rotate/Rotate.jsx
@@ -1,32 +1,14 @@
-function Rotate({verticalFlip, horizontalFlip, leftRotate, rightRotate}) {
+import { Button } from '../Button/Button'
+
+function Rotate({ verticalFlip, horizontalFlip, leftRotate, rightRotate }) {
   return (
     <div className="flex flex-col text-center">
       <h2 className="text-3xl font-extrabold mt-2 text-white mb-4">Rotate</h2>
       <div className="flex flex-col max-w-xs gap-4">
-        <button
-          className="rounded-lg px-4 py-2 capitalize text-white text-sm bg-violet-500 hover:bg-violet-600 active:bg-violet-700 focus:outline-none focus:ring focus:ring-violet-300"
-          onClick={horizontalFlip}
-        >
-          Vertical ↑↓
-        </button>
-        <button
-          className="rounded-lg px-4 py-2 capitalize text-white text-sm bg-violet-500 hover:bg-violet-600 active:bg-violet-700 focus:outline-none focus:ring focus:ring-violet-300"
-          onClick={verticalFlip}
-        >
-          Horizontal →←
-        </button>
-        <button
-          className="rounded-lg px-4 py-2 capitalize text-white text-sm bg-violet-500 hover:bg-violet-600 active:bg-violet-700 focus:outline-none focus:ring focus:ring-violet-300"
-          onClick={leftRotate}
-        >
-          Left Rotate ←
-        </button>
-        <button
-          className="rounded-lg px-4 py-2 capitalize text-white text-sm bg-violet-500 hover:bg-violet-600 active:bg-violet-700 focus:outline-none focus:ring focus:ring-violet-300"
-          onClick={rightRotate}
-        >
-          Right Rotate →
-        </button>
+        <Button onClick={horizontalFlip}>Vertical ↑↓</Button>
+        <Button onClick={verticalFlip}>Horizontal →←</Button>
+        <Button onClick={leftRotate}>Left Rotate ←</Button>
+        <Button onClick={rightRotate}>Right Rotate →</Button>
       </div>
     </div>
   )
